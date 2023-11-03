8f417d96d1e1f2b2d5c4090e403a4248bee35996
Camilo_Peña <cc.pena2@uniandes.edu.co>
2023-11-03 09:58:06 -0500

    Se envia la aplicacion base

---
+++ mynewapp/src/app/Plantas/Plantas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Plantas',
+  templateUrl: './Plantas.component.html',
+  styleUrls: ['./Plantas.component.css']
+})
+export class PlantasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
